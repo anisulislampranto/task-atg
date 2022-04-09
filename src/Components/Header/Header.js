3bc3b35b6de28b5c5792065ea8680ea83500d5ff
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-04-10 05:35:36 +0600

    added minimal toggle nav resposive

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Header.css";
+
+const Header = () => {
+  return (
+    <div className="header col-md-12 col-12">
+      <div className="group-heading">
+        <h2>Computer Engineering</h2>
+        <p>1234426 Engineers follow this</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,33 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Navabr.css";
-import logo from "../../Assests/Screenshot_2022-04-07_at_12.08.46_PM-removebg-preview.png";
+import logo from "../../Assests/logo.png";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
+  faBars,
   faCaretDown,
   faMagnifyingGlass,
 } from "@fortawesome/free-solid-svg-icons";
 
 const Navbar = () => {
-  return (
-    <div style={{ height: "72px" }} className="">
-      <div className=" d-flex space-evenly justify-content-evenly">
-        <img width={"200px"} src={logo} alt="" className="my-auto" />
-        <div style={{ width: "350px" }} class="form-group has-search my-auto">
-          <span class="fa fa-search form-control-feedback">
-            <FontAwesomeIcon icon={faMagnifyingGlass} />
-          </span>
-          <input
-            style={{ borderRadius: "20px", background: "#F2F2F2" }}
-            type="text"
-            class="form-control"
-            placeholder="Search For Your Favorite groups in ATG"
-          />
-        </div>
+  const [toggle, setToggle] = useState(false);
 
-        <p className="my-auto">
-          Create Account. <span className="text-primary">It's Free! </span>
-          <FontAwesomeIcon icon={faCaretDown} />
-        </p>
+  return (
+    <div className="p-3">
+      <div className=" d-flex space-evenly justify-content-evenly navigation-bar my-auto">
+        <div className="d-flex space-evenly justify-content-evenly">
+          <img width={"200px"} src={logo} alt="" className="my-auto" />
+          <span onClick={() => setToggle(!toggle)} className="menu-icon">
+            <FontAwesomeIcon icon={faBars} />
+          </span>
+        </div>
+        <>
+          <div
+            style={{ width: "350px" }}
+            class="form-group has-search my-auto searchBar-big-screen"
+          >
+            <span class="fa fa-search form-control-feedback">
+              <FontAwesomeIcon icon={faMagnifyingGlass} />
+            </span>
+            <input
+              style={{ borderRadius: "20px", background: "#F2F2F2" }}
+              type="text"
+              class="form-control"
+              placeholder="Search For Your Favorite groups in ATG"
+            />
+          </div>
+          <p className="my-auto login-big-screen">
+            Create Account. <span className="text-primary">It's Free! </span>
+            <FontAwesomeIcon icon={faCaretDown} />
+          </p>
+        </>
+
+        {toggle ? (
+          <>
+            <div
+              style={{ width: "350px" }}
+              class="form-group has-search my-auto searchBar-small-screen"
+            >
+              <span class="fa fa-search form-control-feedback">
+                <FontAwesomeIcon icon={faMagnifyingGlass} />
+              </span>
+              <input
+                style={{ borderRadius: "20px", background: "#F2F2F2" }}
+                type="text"
+                class="form-control"
+                placeholder="Search For Your Favorite groups in ATG"
+              />
+            </div>
+            <p className="my-auto login-small-screen">
+              Create Account. <span className="text-primary">It's Free! </span>
+              <FontAwesomeIcon icon={faCaretDown} />
+            </p>
+          </>
+        ) : (
+          <span></span>
+        )}
       </div>
     </div>
   );
